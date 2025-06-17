ec5a1212bc24d4cb6d30b35a9bd13a98eb62c8a5
André Luferat <33375030+Luferat@users.noreply.github.com>
2025-06-17 09:52:00 -0300

    Update CoisaCadastroController.java

--- src/main/java/br/senac/rj/crud/controller/CoisaCadastroController.java
+++ src/main/java/br/senac/rj/crud/controller/CoisaCadastroController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.Valid; // <-- ALTERADO DE 'javax.validation.Valid' PARA 'jakarta.validation.Valid'
+import jakarta.validation.Valid;
 import java.time.LocalDateTime;
 
 @RestController
